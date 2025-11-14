ae3fa543dab889b17e2725f6928ed8833bddb1eb
Ana Carolini de Azevedo Nicoletti <azevedoanacaroliny@gmail.com>
2025-11-14 17:07:53 -0300

    estrutura vetores_string.cpp

---
+++ vetores_string.cpp
@@ -0,0 +1,16 @@
+#include <iostream> 
+#include <string> 
+
+using namespace std;  
+
+int main() 
+
+{ 
+
+    string texto [7] = {"Monique", "Ana", "Lucas", "Aiko", "Coaio", "Miguel", "Five"}; 
+ 
+    cout << texto[1]; 
+
+    return 0; 
+
+} 
